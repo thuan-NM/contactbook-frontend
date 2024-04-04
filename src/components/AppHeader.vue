f1ce5abcd29007d4f1a297fe915a4d79040c2ee0
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2024-04-04 22:32:32 +0700

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ung dung quan ly danh ba</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{name: 'contactbook'}" class="nav-link">
+                Danh ba
+                <i class="fas fa-address-book"></i>
+            </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        props: {
+            contacts: { type: Array, default: []},
+            activeIndex: { type: Number, default: -1 },
+        },
+        emits: ["update:activeIndex"],
+        methods: {
+            updateActiveIndex(index) {
+                this.$emits("update:activeIndex", index);
+            }
+        }
+    };
+</script>
+
+<template>
+    <ul class="list-group">
+        <li
+            class="list-group-item"
+            v-for="(contact, index) in contact"
+            :key="contact._id"
+            :class="{ active: index === activeIndex }"
+            @click="updateActiveIndex(index)"
+        >
+            {{ contact.name }}
+        </li>
+    </ul>
+</template>
